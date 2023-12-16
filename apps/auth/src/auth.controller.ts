b369d3ed1844daf24285cf1e38e22093fde0a04c
ahmed-afzal1 <afzalbhairab4@gmail.com>
2023-12-16 11:20:15 +0600

    chore: add authentication functionality

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { UserDocument } from './users/models/user.schema';
+import { Response } from 'express';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
+  @Post()
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.authService.login(user, response);
+    response.send(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  async getUser(@CurrentUser() user: UserDocument) {
+    return user;
   }
 }
